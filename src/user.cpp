388340e554a46f0e17dd4ad2d9a0c313b3208a87
Himanshu Sah <himanshusah41@gmail.com>
2023-11-06 23:35:55 -0500

    Gitignore added

---
+++ src/user.cpp
@@ -0,0 +1,11 @@
+/*
+ * This source file works with user creation, login, informations, accounts, loans, admin, and deletion if required
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello from user";
+    return 0;
+}
